7a762a911f31fbcf9f086527a93fed3563d38e64
KilianHoefman <kilian.hoefman@student.hogent.be>
2020-04-02 11:59:47 +0200

    Get en setters voor Aankondiging en academiejaar

--- ITLabNET/Models/Domain/Academiejaar.cs
+++ ITLabNET/Models/Domain/Academiejaar.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ITLabNET.Models.Domain.Sessies;
+using System;
 using System.Collections.Generic;
 
 namespace ITLabNET.Models.Domain
@@ -14,6 +15,68 @@ namespace ITLabNET.Models.Domain
         private string _startString;
         private string _eindString;
         private int _aantal;
+        #endregion
+
+        #region Properties
+        public int AcademieJaar
+        {
+            get
+            {
+                return _academiejaar;
+            }
+            set
+            {
+                if (value < 2000 || value > 3000)
+                    throw new ArgumentException("Academiejaar moet na 2000 zijn en voor 3000");
+                _academiejaar = value;
+            }
+        }
+
+        public DateTime Start
+        {
+            get
+            {
+                return _start;
+            }
+            set
+            {
+                if (value == null)
+                    throw new ArgumentException("Starttijdstip moet ingevuld zijn");
+                _start = value;
+            }
+        }
+
+        public DateTime Eind
+        {
+            get
+            {
+                return _eind;
+            }
+            set
+            {
+                if (value == null)
+                    throw new ArgumentException("Eindtijdstip moet ingevuld zijn");
+                _eind = value;
+            }
+        }
+
+        public int Aantal
+        {
+            get
+            {
+                return _aantal;
+            }
+            set
+            {
+                if (value < 0)
+                    throw new ArgumentException("Aantal mag niet onder nul liggen");
+                _aantal = value;
+            }
+        }
+        #endregion
+
+        #region constructors
+        public Academiejaar() { }
         #endregion
     }
 }
