c16295259ca809a316dbed5f71175c60ecf81f3d
KilianHoefman <kilian.hoefman@student.hogent.be>
2020-04-29 23:35:46 +0200

    Verandering voor login pagina

--- ITLabNET/Areas/Identity/Pages/Account/Login.cshtml.cs
+++ ITLabNET/Areas/Identity/Pages/Account/Login.cshtml.cs
@@ -50,7 +50,7 @@ namespace ITLabNET.Areas.Identity.Pages.Account
             [DataType(DataType.Password)]
             public string Password { get; set; }
 
-            [Display(Name = "Remember me?")]
+            [Display(Name = "Onthoud keuze")]
             public bool RememberMe { get; set; }
         }
 
